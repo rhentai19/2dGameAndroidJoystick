58f4a1e2189f11892adbe001e179db23a316abbe
rhentai19 <89902258+rhentai19@users.noreply.github.com>
2022-09-12 13:21:50 -0500

    Player Barra de daño2

---
+++ app/src/main/java/com/example/a2dgamedevelopment/object/HealthBar.java
@@ -0,0 +1,58 @@
+package com.example.a2dgamedevelopment.object;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.a2dgamedevelopment.R;
+
+public class HealthBar {
+
+    private final Player player;
+    private final int  width , height, margin;
+    private final Paint borderPaint, healthPaint;
+
+
+    public HealthBar(Context context, Player player){
+        this.player = player;
+        this.width = 100;
+        this.height = 20;
+        this.margin = 2;
+
+        this.borderPaint = new Paint();
+        int borderColor = ContextCompat.getColor(context, R.color.healthBarBorder);
+        borderPaint.setColor(borderColor);
+
+        this.healthPaint = new Paint();
+        int healthColor = ContextCompat.getColor(context, R.color.healthBar);
+        healthPaint.setColor(healthColor);
+    }
+
+    public void draw(Canvas canvas){
+        float x = (float) player.getPositionX();
+        float y = (float) player.getPositionY();
+        float distanceToPlayer = 30;
+        float healthPointPercentage =(float) player.getHealthPoints()/player.MAX_HEALTH_POINTS;
+
+        float borderLeft,borderTop,borderRight,borderBottom;
+        borderLeft = x - width/2f;
+        borderRight = x + width/2f;
+        borderBottom = y - distanceToPlayer;
+        borderTop = borderBottom - height;
+        canvas.drawRect(borderLeft,borderTop,borderRight,borderBottom,borderPaint);
+
+        float healthLeft,healthTop,healthRight,healthBottom,healthWidth,healthHeight;
+        healthWidth = width -2*margin;
+        healthHeight = height - 2*margin;
+        healthLeft = borderLeft + margin;
+        healthRight = healthLeft + healthWidth*healthPointPercentage;
+        healthBottom = borderBottom - margin;
+        healthTop = healthBottom - healthHeight;
+
+
+
+        canvas.drawRect(healthLeft,healthTop,healthRight,healthBottom,healthPaint);
+    }
+}
